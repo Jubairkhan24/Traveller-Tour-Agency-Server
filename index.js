7e1185ec8ebe8c5100cd3faa74725681292da82c
Jubair Istiaq Khan <superanonto001@gmail.com>
2021-10-31 18:51:51 +0600

    issue issue

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
@@ -13,16 +13,17 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.raopv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-// console.log(uri)
+console.log(uri)
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
     try {
         await client.connect();
-        // console.log("connected to database")
         const database = client.db('travellerAgency');
         const servicesCollection = database.collection('services');
+
+        console.log('all are working')
 
         // GET API
         app.get('/services', async (req, res) => {
@@ -66,18 +67,11 @@ async function run() {
 
 run().catch(console.dir);
 
-// app.get('/', (req, res) => {
-//     res.send('Running Genius Server');
-// });
-
-// app.get('/hello', (req, res) => {
-//     res.send('hello updated here')
-// })
-
-app.listen(port, () => {
-    console.log('Running Genius Server on port', port);
-})
-
 app.get('/', (req, res) => {
     res.send('running my crud server')
 })
+
+app.listen(port, () => {
+    console.log('Running traveller Server on port', port);
+})
+
