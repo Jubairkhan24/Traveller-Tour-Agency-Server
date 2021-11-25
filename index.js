9d65a0ba5977b2d7246dea42b6676054a33ab7c8
Jubair Istiaq Khan <superanonto001@gmail.com>
2021-11-25 15:35:12 +0600

    final commit

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ async function run() {
         app.get('/services', async (req, res) => {
             const cursor = servicesCollection.find({});
             const services = await cursor.toArray();
-            // console.log('this is editable comment')
             res.send(services);
         });
 
